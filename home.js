68c35a355f1162a42a73a480d817e0bbbdb5ddc2
Morimx <82421851+Morimx@users.noreply.github.com>
2021-06-10 22:46:59 -0300

    Empezando el curso

---
+++ home.js
@@ -0,0 +1,13 @@
+var ingreseprimernumero = parseInt(prompt("Ingrese un numero entero"));
+var ingresesegundonumero = parseInt(prompt("Ingrese el segundo numero entero"));
+var suma = ingreseprimernumero + ingresesegundonumero;
+var resultado = suma > 10;
+var igual = suma == 10;
+
+if (resultado){
+alert("Tu numero es mayor a 10!");
+}else if(resultado = 10){
+    alert("tu numero es igual a 10")
+    }else{ 
+alert("Tu numero es menor a 10!");
+}
