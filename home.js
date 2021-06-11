5fab3f38e6f23f6cb89736c8ca92197e5dd737e7
Morimx <82421851+Morimx@users.noreply.github.com>
2021-06-10 23:24:31 -0300

    arreglando

--- home.js
+++ home.js
@@ -2,11 +2,11 @@ var ingreseprimernumero = parseInt(prompt("Ingrese un numero entero"));
 var ingresesegundonumero = parseInt(prompt("Ingrese el segundo numero entero"));
 var suma = ingreseprimernumero + ingresesegundonumero;
 var resultado = suma > 10;
-var igual = suma == 10;
+var igual = suma == 10
 
 if (resultado){
 alert("Tu numero es mayor a 10!");
-}else if(resultado = 10){
+}else if(resultado = igual){
     alert("tu numero es igual a 10")
     }else{ 
 alert("Tu numero es menor a 10!");
