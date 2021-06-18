47860d0a98e0d13111a92c95977c2b0267a5e8f7
Morimx <82421851+Morimx@users.noreply.github.com>
2021-06-17 21:00:51 -0300

    Ciclos

--- home.js
+++ home.js
@@ -1,11 +1,32 @@
-var ingreseprimernumero = parseInt(prompt("Ingrese un numero entero"));
-var ingresesegundonumero = parseInt(prompt("Ingrese el segundo numero entero"));
-var suma = ingreseprimernumero + ingresesegundonumero;
+var ingProducto = parseInt(prompt("Ingrese el numero del producto deseado\n 1) Coca cola \n 2) Pepsi \n 3) Seven-up \n 4) Cancelar"))
+var StockCocacola = 10
+var StockPepsi = 10
+var StockSevenup = 10
 
-if (suma > 10){
-alert("Tu numero es mayor a 10!");
-}else if(suma === 10){
-    alert("tu numero es igual a 10");
-    }else{ 
-alert("Tu numero es menor a 10!");
+
+while(ingProducto != "4"){
+   switch (ingProducto) {
+       case 1:
+           if(StockCocacola > 0){
+            StockCocacola -= 1;
+            alert(`Seleccionaste coca cola, nos quedan ${StockCocacola} en stock`);}
+            break;
+        case 2:
+            StockPepsi -= 1;
+            alert(`Seleccionaste Pepsi, nos quedan ${StockPepsi} en stock`);
+            break;
+        case 3:
+            StockSevenup -= 1;
+            alert(`Seleccionaste Seven-up, nos quedan ${StockSevenup} en stock`)
+            break;
+       default:
+        alert(`Selecciona un producto o escribi Cancelar para finalizar`);   
+        break;
+   }
+   ingProducto = parseInt(prompt("Ingrese el numero del producto deseado\n 1) Coca cola \n 2) Pepsi \n 3) Seven-up \n 4) Cancelar"))
 }
+
+console.log("Quedan actualmente en stock:")
+console.log(StockCocacola)
+console.log(StockPepsi)
+console.log(StockSevenup)
