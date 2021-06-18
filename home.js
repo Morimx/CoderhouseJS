d0da2795192d3efcd6a8665db9313d1539fd1601
Morimx <82421851+Morimx@users.noreply.github.com>
2021-06-17 21:25:33 -0300

    Cambios

--- home.js
+++ home.js
@@ -14,15 +14,26 @@ while(ingProducto != "4"){
            if(StockCocacola > 0){
             StockCocacola -= 1;
             alert(`Seleccionaste coca cola, nos quedan ${StockCocacola} en stock`);}
-            break;
+            else{
+                alert("No tenemos mas stock de este producto")
+              }
+              break;
         case 2:
-            StockPepsi -= 1;
-            alert(`Seleccionaste Pepsi, nos quedan ${StockPepsi} en stock`);
-            break;
+            if(StockPepsi > 0){
+                StockPepsi -= 1;
+                alert(`Seleccionaste Pepsi, nos quedan ${StockPepsi} en stock`);}
+                else{
+                    alert("No tenemos mas stock de este producto")
+                  }
+                  break;
         case 3:
-            StockSevenup -= 1;
-            alert(`Seleccionaste Seven-up, nos quedan ${StockSevenup} en stock`)
-            break;
+            if(StockSevenup > 0){
+                StockSevenup -= 1;
+                alert(`Seleccionaste Seven-up, nos quedan ${StockSevenup} en stock`);}
+                else{
+                    alert("No tenemos mas stock de este producto")
+                  }
+                  break;
        default:
         alert(`Selecciona un producto o seleecione 4 para salir`);   
         break;
