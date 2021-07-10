e24f24a72afcf2c32cacc60033f84fc848921d6b
Morimx <82421851+Morimx@users.noreply.github.com>
2021-07-10 20:37:48 -0300

    Utilize math round para las cuotas

--- functions/calccuotas.js
+++ functions/calccuotas.js
@@ -1,7 +1,7 @@
 //Funcion para display de cuotas
 function mostrarCuotas(cantidadCuotas) {
     if (cuotas[cantidadCuotas]) {
-        cantCuotas.innerHTML = resultadofinal / cuotas[cantidadCuotas];
+        cantCuotas.innerHTML = Math.round(resultadofinal / cuotas[cantidadCuotas]);
         return;
     }
     return alert(`La cantidad de cuotas ${cantidadCuotas} no esta disponible`);
